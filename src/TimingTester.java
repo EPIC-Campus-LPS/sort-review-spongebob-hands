fcf04bc6dadcca8ca9a0059b6ca43f76322f33b0
mariev718_lpsk12 <mariev718@lpsk12.org>
2023-09-20 11:53:28 -0600

    timing tester

--- src/TimingTester.java
+++ src/TimingTester.java
@@ -1,15 +1,65 @@
+/**
+ * Times 4 sorting algorithms and returns results for each
+ *
+ * @author Connor Bonn and Marie Viita
+ * @version 1.0, 09/20/2023
+ */
 public class TimingTester {
     public static void main(String[] args){
 
-        int[] small = {1, 5, 8, 3, 9};
+        //creates new array of size user chooses
+        int[] arr = new int[50000000];
 
-        long startTime = System.currentTimeMillis();
+        // randomizes each value in array
+        for(int i = 0; i < arr.length; i++) {
 
-        QuadSorts.bubbleSort(small);
+            arr[i] = (int) (Math.random() * 1000 + 1);
 
-        long endTime = System.currentTimeMillis();
+        }
 
-        System.out.print(endTime-startTime);
+        // records start time
+        long bubbleST = System.currentTimeMillis();
+        // sorts
+        QuadSorts.bubbleSort(arr);
+        // records end time
+        long bubbleET = System.currentTimeMillis();
+        // prints the time it took to sort
+        System.out.println("Bubble Sort: " + (bubbleET - bubbleST));
+
+        // randomizes array
+        for(int i = 0; i < arr.length; i++) {
+
+            arr[i] = (int) (Math.random() * 1000 + 1);
+
+        }
+
+        // repeats for each sorting algorithm
+        long selectionST = System.currentTimeMillis();
+        QuadSorts.selectionSort(arr);
+        long selectionET = System.currentTimeMillis();
+        System.out.println("Selection Sort: " + (selectionET - selectionST));
+
+        for(int i = 0; i < arr.length; i++) {
+
+            arr[i] = (int) (Math.random() * 1000 + 1);
+
+        }
+
+        long insertST = System.currentTimeMillis();
+        QuadSorts.insertionSort(arr);
+        long insertET = System.currentTimeMillis();
+        System.out.println("Insertion Sort: " + (insertET - insertST));
+
+        for(int i = 0; i < arr.length; i++) {
+
+            arr[i] = (int) (Math.random() * 1000 + 1);
+
+        }
+
+        long mergeST = System.currentTimeMillis();
+        QuadSorts.mergeSort(arr);
+        long mergeET = System.currentTimeMillis();
+        System.out.println("Merge Sort: " + (mergeET - mergeST));
 
     }
 }
